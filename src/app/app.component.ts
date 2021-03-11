2ab4389dae812263fd04b923b7ca25c382310eb1
Shubham Saxena <shubhsaxena22@gmail.com>
2021-03-11 19:41:10 +0530

    added all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyApp2';
+  // title = 'MyApp2';
+  // total: number
+  // prop_from_parent: string = "property from root parent"
+
+  // add(event_data: number) {
+  //   this.total = event_data + 10;
+  //   alert("data recieved from parent")
+  // }
+
+  // str(event_d: string) {
+  //   alert(`data recived is + ${event_d}`)
+  // }
 }
