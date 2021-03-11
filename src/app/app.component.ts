6f89a2caf38a08a507775c9548c44be38338272e
Shubham Saxena <shubhsaxena22@gmail.com>
2021-03-11 20:02:08 +0530

    removed comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // title = 'MyApp2';
-  // total: number
-  // prop_from_parent: string = "property from root parent"
+  title = 'MyApp2';
 
-  // add(event_data: number) {
-  //   this.total = event_data + 10;
-  //   alert("data recieved from parent")
-  // }
-
-  // str(event_d: string) {
-  //   alert(`data recived is + ${event_d}`)
-  // }
 }
