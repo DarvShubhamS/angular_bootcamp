1b3800a5ef2f8ace797b28aaeb0edf8f3f1c77f9
Shubham Saxena <shubhsaxena22@gmail.com>
2021-03-17 16:22:01 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms'
+import { Product } from './product.service'
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductComponent } from './product/product.component';
+
+
+//setting up routes
+const MyRoutes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard', component: DashboardComponent
+  },
+  {
+    path: 'create-product', component: ProductComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(MyRoutes)
   ],
-  providers: [],
+  providers: [Product],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
