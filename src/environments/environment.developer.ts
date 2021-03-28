2504e1ab2ef88a0e680aafb8e00ec5fb5d04e2cc
Shubham Saxena <shubhsaxena22@gmail.com>
2021-03-28 18:45:47 +0530

    added missing comment

--- src/environments/environment.developer.ts
+++ src/environments/environment.developer.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    // setting developement http url
+    // setting developement http url and username
     hostUrl: 'http://localhost:5500/',
     username: 'Developer'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  // setting production http url and username
   hostUrl: 'http://localhost:5000/',
   username: 'userProd'
 
